2e9ce09c08d036b6e475a89dd90859a75200b374
thaidoandat <thai.doan.dat@sun-asterisk.com>
2021-07-14 14:44:04 +0700

    Chapter 7: Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,24 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    return render :new unless @user.save
+
+    flash[:success] = t "welcome"
+    redirect_to @user
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+          .permit :name, :email, :password, :password_confirmation
+  end
 end
